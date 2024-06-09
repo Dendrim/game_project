6234150ffe82281cd971ef5c9e99597562fa51d4
Dendrim <Dendrim97@gmail.com>
2024-06-09 20:14:18 +0300

    main.lua cleanup

--- main.lua
+++ main.lua
@@ -1,1 +1,17 @@
--- initial commit
+require "utils/dump"
+require "utils/vectors"
+require "utils/bind_function"
+
+function love.load()
+  local success = love.window.setMode( 800, 600, {vsync = 0} )
+end
+
+function love.update(dt)
+
+end
+
+function love.draw()
+  love.graphics.print(love.mouse.getX(), 20, 270)
+  love.graphics.print(love.mouse.getY(), 20, 280)
+  love.graphics.print(1/love.timer.getDelta(), 20, 290)
+end
